1094e191857c9780c7132e0e5c515bd946edbdda
André Mathisen <9752808+anzure@users.noreply.github.com>
2021-01-17 16:15:34 +0100

    📱 Mobilvennlig søkdesign.
    Sentrert søkesiden og forbedret mobilvennlighet.

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -1,34 +1,42 @@
-import React, { useState, useRef } from 'react';
-import axios from 'axios';
-import { Container } from '@material-ui/core';
-import SearchInput from './components/SearchInput';
-import SearchResult from './components/SearchResult';
+import React, { useState, useRef } from "react";
+import axios from "axios";
+import { Grid } from "@material-ui/core";
+import SearchInput from "./components/SearchInput";
+import SearchResult from "./components/SearchResult";
 
 export default function Search() {
+  const isSearching = useRef();
+  const [searchResult, setSearchResult] = useState([]);
 
-    const isSearching = useRef();
-    const [searchResult, setSearchResult] = useState([]);
+  async function searchQuery(query) {
+    setSearchResult([]);
+    const currentSearch = Date.now();
+    isSearching.current = currentSearch;
 
-    async function searchQuery(query) {
+    await axios("https://api.ehfsok.no/search?query=" + query)
+      .then((result) => {
+        if (currentSearch === isSearching.current) setSearchResult(result.data);
+      })
+      .catch((error) => {
+        console.warn("Failed to connect to API for search!");
         setSearchResult([]);
-        const currentSearch = Date.now();
-        isSearching.current = currentSearch;
+      });
+  }
 
-        await axios(
-            'https://api.ehfsok.no/search?query=' + query
-        ).then((result) => {
-            if (currentSearch === isSearching.current)
-                setSearchResult(result.data);
-        }).catch((error) => {
-            console.warn("Failed to connect to API for search!");
-            setSearchResult([]);
-        });
-    }
-
-    return (
-        <Container>
-            <SearchInput query={searchQuery} />
-            <SearchResult result={searchResult} />
-        </Container>
-    )
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+    >
+      <Grid item>
+        <SearchInput query={searchQuery} />
+      </Grid>
+      <Grid item>
+        <SearchResult result={searchResult} />
+      </Grid>
+    </Grid>
+  );
 }
--- src/views/components/SearchResult.jsx
+++ src/views/components/SearchResult.jsx
@@ -1,42 +1,34 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-
-const useStyles = makeStyles((theme) => ({
-    table: {
-        width: "30%",
-    },
-}));
+import React from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
 export default function SearchResult(props) {
-    const classes = useStyles();
-    const searchResult = props.result;
+  const searchResult = props.result;
 
-    return (
-        <TableContainer>
-            <Table className={classes.table}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Org nr</TableCell>
-                        <TableCell align="right">Bedriftsnavn</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {searchResult.map((result) => (
-                        <TableRow key={result.organizationNumber}>
-                            <TableCell component="th" scope="row">
-                                {result.organizationNumber}
-                            </TableCell>
-                            <TableCell align="right">{result.name}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    )
+  return (
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Org nr</TableCell>
+            <TableCell align="right">Bedriftsnavn</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {searchResult.map((result) => (
+            <TableRow key={result.organizationNumber}>
+              <TableCell component="th" scope="row">
+                {result.organizationNumber}
+              </TableCell>
+              <TableCell align="right">{result.name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
